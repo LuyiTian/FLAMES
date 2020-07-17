0c875ad141af26561ab4a3783d5e90f3039572b7
Luyi Tian <tlytiger@hotmail.com>
2020-07-17 20:57:39 +1000

    bug fix

--- python/filter_gff.py
+++ python/filter_gff.py
@@ -1,7 +1,7 @@
 # filter gff file after realign to transcript.
 from parse_gene_anno import parse_gff_tree
 from collections import Counter
-
+import copy
 
 def annotate_full_splice_match(transcript_to_exon,transcript_to_exon_ref,transcript_dict,transcript_dict_ref,anno_out,tr_cnt,min_sup_reads):
     splice_dict = {}
@@ -80,39 +80,43 @@ def annotate_full_splice_match(transcript_to_exon,transcript_to_exon_ref,transcr
 
 def annotate_filter_gff(isoform_gff,ref_gff,isoform_out,anno_out,tr_cnt,min_sup_reads):
     """
-    combine FLAMES ouput with reference and filter out transcript by
+    combine FLTSA ouput with reference and filter out transcript by
     realignment result
     """
     gff3_fmt = "{_ch}\t{_sr}\t{_ty}\t{_st}\t{_en}\t{_sc}\t{_stnd}\t{_ph}\t{_attr}"
 
     chr_to_gene, transcript_dict, gene_to_transcript, transcript_to_exon = parse_gff_tree(isoform_gff)
-    _, transcript_dict_ref, gene_to_transcript_ref, transcript_to_exon_ref = parse_gff_tree(ref_gff)
+    chr_to_gene_ref, transcript_dict_ref, gene_to_transcript_ref, transcript_to_exon_ref = parse_gff_tree(ref_gff)
     gff_rec = []
     for ch in chr_to_gene:
-        for ge in chr_to_gene[ch]:
+        new_ge_list = copy.deepcopy(chr_to_gene[ch])
+        new_ge_list.extend(chr_to_gene_ref[ch])
+        new_ge_list = list(set(new_ge_list))
+        for ge in new_ge_list:
             gff_tmp = []
             total_cnt = 0
             mi = 99999999999
             ma = -1
-            for tr in gene_to_transcript[ge]:
-                if (tr in tr_cnt) and (tr_cnt[tr]>=min_sup_reads):
-                    gff_tmp.append(gff3_fmt.format(_ch=ch,_sr="FLAMES",_ty="transcript",
-                        _st=transcript_to_exon[tr][0][0]+1,
-                        _en=transcript_to_exon[tr][-1][1],
-                        _sc=".",_stnd=transcript_dict[tr].strand, _ph=".",
-                        _attr="ID=transcript:{};transcript_id={};Parent=gene:{};support_count={}".format(tr,tr, ge, tr_cnt[tr])))
-                    total_cnt += tr_cnt[tr]
-                    mi = min(mi,transcript_to_exon[tr][0][0])
-                    ma = max(ma,transcript_to_exon[tr][-1][1])
-                    exon_idx = 1
-                    for ex in transcript_to_exon[tr]:
-                        gff_tmp.append(gff3_fmt.format(_ch=ch,_sr="FLAMES",_ty="exon",_st=ex[0]+1,_en=ex[1], # `+1` because gff is 1-based
-                        _sc=".",_stnd=transcript_dict[tr].strand, _ph=".",
-                        _attr="exon_id=exon:{}_{};Parent=transcript:{};rank={}".format(ex[0]+1, ex[1],tr, exon_idx )))
-                        exon_idx += 1
+            if ge in gene_to_transcript:
+                for tr in gene_to_transcript[ge]:
+                    if (tr in tr_cnt) and (tr_cnt[tr]>=min_sup_reads):
+                        gff_tmp.append(gff3_fmt.format(_ch=ch,_sr="FLTSA",_ty="transcript",
+                            _st=transcript_to_exon[tr][0][0]+1,
+                            _en=transcript_to_exon[tr][-1][1],
+                            _sc=".",_stnd=transcript_dict[tr].strand, _ph=".",
+                            _attr="ID=transcript:{};transcript_id={};Parent=gene:{};support_count={}".format(tr,tr, ge, tr_cnt[tr])))
+                        total_cnt += tr_cnt[tr]
+                        mi = min(mi,transcript_to_exon[tr][0][0])
+                        ma = max(ma,transcript_to_exon[tr][-1][1])
+                        exon_idx = 1
+                        for ex in transcript_to_exon[tr]:
+                            gff_tmp.append(gff3_fmt.format(_ch=ch,_sr="FLTSA",_ty="exon",_st=ex[0]+1,_en=ex[1], # `+1` because gff is 1-based
+                            _sc=".",_stnd=transcript_dict[tr].strand, _ph=".",
+                            _attr="exon_id=exon:{}_{};Parent=transcript:{};rank={}".format(ex[0]+1, ex[1],tr, exon_idx )))
+                            exon_idx += 1
             if ge in gene_to_transcript_ref:
                 for tr in gene_to_transcript_ref[ge]:
-                    if (tr not in transcript_dict) and (tr in tr_cnt) and (tr_cnt[tr]>=min_sup_reads):  # not in FLAMES output but in tr count
+                    if (tr not in transcript_dict) and (tr in tr_cnt) and (tr_cnt[tr]>=min_sup_reads):  # not in FLTSA output but in tr count
                         gff_tmp.append(gff3_fmt.format(_ch=ch,_sr="reference",_ty="transcript",
                             _st=transcript_to_exon_ref[tr][0][0]+1,
                             _en=transcript_to_exon_ref[tr][-1][1],
@@ -127,11 +131,18 @@ def annotate_filter_gff(isoform_gff,ref_gff,isoform_out,anno_out,tr_cnt,min_sup_
                             _sc=".",_stnd=transcript_dict_ref[tr].strand, _ph=".",
                             _attr="exon_id=exon:{}_{};Parent=transcript:{};rank={}".format(ex[0]+1, ex[1],tr, exon_idx )))
                             exon_idx += 1
-            if len(gff_tmp)>0:
-                gff_tmp.insert(0,gff3_fmt.format(_ch=ch,_sr="FLAMES",_ty="gene",
+            if len(gff_tmp)>0 and ge in gene_to_transcript:
+                gff_tmp.insert(0,gff3_fmt.format(_ch=ch,_sr="FLTSA",_ty="gene",
                     _st=mi+1,
                     _en=ma,
                     _sc=".",_stnd=transcript_dict[gene_to_transcript[ge][0]].strand, _ph=".",
+                    _attr="ID=gene:{};gene_id={};support_count={}".format(ge, ge, total_cnt)))
+                gff_rec.extend(gff_tmp)
+            elif len(gff_tmp)>0:
+                gff_tmp.insert(0,gff3_fmt.format(_ch=ch,_sr="FLTSA",_ty="gene",
+                    _st=mi+1,
+                    _en=ma,
+                    _sc=".",_stnd=transcript_dict_ref[gene_to_transcript_ref[ge][0]].strand, _ph=".",
                     _attr="ID=gene:{};gene_id={};support_count={}".format(ge, ge, total_cnt)))
                 gff_rec.extend(gff_tmp)
     iso_annotated = open(isoform_out,"w")
